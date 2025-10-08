1cb1ae39573198ee744c72c0eba4f169ac2d182d
Ozan Gunalp <ozangunalp@gmail.com>
2025-10-08 15:58:35 +0200

    Move rag store dev service to the new model
    the dev service is started only when it is needed

--- runtime-dev/src/main/java/io/quarkiverse/chappie/runtime/dev/ChappieRecorder.java
+++ runtime-dev/src/main/java/io/quarkiverse/chappie/runtime/dev/ChappieRecorder.java
@@ -1,7 +1,11 @@
 package io.quarkiverse.chappie.runtime.dev;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.SubmissionPublisher;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import io.quarkus.arc.runtime.BeanContainer;
 import io.quarkus.runtime.RuntimeValue;
@@ -11,11 +15,20 @@ import io.quarkus.runtime.annotations.Recorder;
 public class ChappieRecorder {
 
     public RuntimeValue<SubmissionPublisher<String>> createChappieServerManager(BeanContainer beanContainer,
-            ChappieAssistant assistant,
-            String chappieServerVersion,
-            Map<String, String> chappieRAGProperties) {
+            ChappieAssistant assistant, String chappieServerVersion) {
+        Config config = ConfigProvider.getConfig();
+        Map<String, String> chappieRAGProperties = new HashMap<>();
+        configMap(config, chappieRAGProperties, "quarkus.datasource.chappie.db-kind");
+        configMap(config, chappieRAGProperties, "quarkus.datasource.chappie.jdbc.url");
+        configMap(config, chappieRAGProperties, "quarkus.datasource.chappie.username");
+        configMap(config, chappieRAGProperties, "quarkus.datasource.chappie.password");
+        configMap(config, chappieRAGProperties, "quarkus.datasource.chappie.active");
 
         ChappieServerManager chappieServerManager = beanContainer.beanInstance(ChappieServerManager.class);
         return new RuntimeValue(chappieServerManager.init(chappieServerVersion, assistant, chappieRAGProperties));
+    }
+
+    void configMap(Config config, Map<String, String> map, String key) {
+        map.put(key, config.getValue(key, String.class));
     }
 }
